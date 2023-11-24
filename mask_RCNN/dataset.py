b718b82aa6261cc9c95d554dfb9b82c782feb458
Muhammad Tarek Refaat <32721009+muhammadtarek98@users.noreply.github.com>
2023-11-25 01:01:49 +0200

    Add files via upload

--- mask_RCNN/dataset.py
+++ mask_RCNN/dataset.py
@@ -42,43 +42,29 @@ class CustomDataSet(Dataset):
 
     def __getitem__(self, index):
         # load images and masks
-        img_path = os.path.join(self.images_dir, self.df["images_dir"].iloc[index])
+        image_dir = os.path.join(self.images_dir, self.df["images_dir"].iloc[index])
         mask_dir = os.path.join(self.masks_dir, self.df["masks_dir"].iloc[index])
-        img = read_image(img_path)
-        mask = read_image(mask_dir)
-        # instances are encoded as different colors
-        obj_ids = torch.unique(mask)
-        # first id is the background, so remove it
-        obj_ids = obj_ids[1:]
-        num_objs = len(obj_ids)
-        masks = (mask == obj_ids[:, None, None]).to(dtype=torch.uint8)
-
-        # get bounding box coordinates for each mask
-        boxes = masks_to_boxes(masks)
-
-        # there is only one class
-        labels = torch.ones((num_objs,), dtype=torch.int64)
-
-        image_id = index
-        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # suppose all instances are not crowd
-        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
-
-        # Wrap sample and targets into torchvision tv_tensors:
-        img = tv_tensors.Image(img)
-
-        target = dict()
-        target["boxes"] = tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=F.get_size(img)).squeeze(1)
-        target["masks"] = tv_tensors.Mask(masks)
-        target["labels"] = labels
-        target["image_id"] = image_id
-        target["area"] = area
-        target["iscrowd"] = iscrowd
-
+        json_dir=os.path.join(self.json_dirs,self.df["json_dir"].iloc[index])
+        json_data=CustomDataSet.open_json_file(file_dir=json_dir)
+        image=read_image(path=image_dir)
+        mask=read_image(path=mask_dir)
+        objects_id=torch.unique(mask)
+        objects_id=objects_id[1:]
+        number_of_objects=len(objects_id)
+        masks = (mask == objects_id[:, None, None]).to(dtype=torch.uint8)
+        boxes=masks_to_boxes(masks=masks)
+        labels=torch.ones(size=(number_of_objects,),dtype=torch.int64)
+        #image_id=index
+        target=dict()
+        target["boxes"]=tv_tensors.BoundingBoxes(boxes, format="XYXY", canvas_size=F.get_size(image))
+        target["masks"]=tv_tensors.Mask(masks)
+        target["labels"]=labels
         if self.transforms is not None:
-            img = self.transforms(img)
+            image = self.transforms(image)
+        return image,[target]
 
-        return img, [target]
+
+
 
 
 """
@@ -127,6 +113,6 @@ def get_transform(train):
     transforms = []
     if train:
         transforms.append(torchvision.transforms.RandomHorizontalFlip(0.5))
-    transforms.append(torchvision.transforms.v2.ToDtype(torch.float, scale=True))
-    transforms.append(torchvision.transforms.v2.ToPureTensor())
+    #transforms.append(torchvision.transforms.v2.ToDtype(torch.float, scale=True))
+    transforms.append(torchvision.transforms.ToTensor())
     return torchvision.transforms.Compose(transforms)
---
+++ mask_RCNN/dataset_v2.py
@@ -0,0 +1,77 @@
+import json
+import os
+import numpy as np
+import pandas as pd
+import torch
+import torchvision
+from PIL import Image
+from torch.utils.data import Dataset
+from shapely.geometry import Polygon
+from torchvision.io import read_image
+from torchvision.ops.boxes import masks_to_boxes
+from torchvision import tv_tensors
+from torchvision.transforms.v2 import functional as F
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "masks"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "masks"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "images", self.imgs[idx])
+        mask_path = os.path.join(self.root, "masks", self.masks[idx])
+        img = read_image(img_path)
+        mask = read_image(mask_path)
+        # instances are encoded as different colors
+        obj_ids = torch.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+        num_objs = len(obj_ids)
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = (mask == obj_ids[:, None, None]).to(dtype=torch.uint8)
+
+        # get bounding box coordinates for each mask
+        boxes = masks_to_boxes(masks)
+        boxes = torch.as_tensor(boxes, dtype=torch.int32)
+        print(boxes)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+
+        image_id = idx
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        # Wrap sample and targets into torchvision tv_tensors:
+        img = tv_tensors.Image(img)
+
+        target = {}
+        target["boxes"] = torchvision.ops.boxes.masks_to_boxes(masks=masks)
+        target["masks"] = tv_tensors.Mask(masks)
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img= self.transforms(img)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+def get_transform(train):
+    transforms = []
+    if train:
+        transforms.append(torchvision.transforms.RandomHorizontalFlip(0.5))
+    transforms.append(torchvision.transforms.v2.ToDtype(torch.float, scale=True))
+    #transforms.append(torchvision.transforms.ToTensor())
+    return torchvision.transforms.Compose(transforms)
